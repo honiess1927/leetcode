99ebb127f28574e37425f5a3046f4e9c39361c2e
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-03-12 23:13:19 -0700

    update 287.find-the-duplicate-number.209766576.ac.java

---
+++ 287.find-the-duplicate-number.209766576.ac.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int l = 1, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            int count = 0;
+            for (int num: nums) {
+                if (num <= mid) count++;
+            }
+            if (count <= mid) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+}
