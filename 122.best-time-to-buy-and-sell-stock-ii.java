74fa9d2560c20f9ffe35498cf9517b46c8a29f79
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-03-23 16:34:18 -0700

    update 122.best-time-to-buy-and-sell-stock-ii.java

--- 122.best-time-to-buy-and-sell-stock-ii.java
+++ 122.best-time-to-buy-and-sell-stock-ii.java
@@ -1,10 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
     	if (prices.length < 2) return 0;
-    	int cur = Integer.MAX_VALUE, res = 0;
-    	for (int price : prices) {
-    		if (price > cur) res += price - cur;
-    		cur = price;
+    	int res = 0;
+    	for (int i = 1; i < prices.length; i++) {
+    		if (prices[i] > prices[i - 1]) res += prices[i] - prices[i - 1];
     	}
         return res;
     }
