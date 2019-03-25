659076b78b164a0f4f8c360b8ba2dac60a13c4bc
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-03-24 19:00:58 -0700

    update 444.sequence-reconstruction.java

--- 444.sequence-reconstruction.java
+++ 444.sequence-reconstruction.java
@@ -1,23 +1,19 @@
 class Solution {
 	Map<Integer, List<Integer>> map = new HashMap<>();
 	public boolean sequenceReconstruction(int[] org, List<List<Integer>> seqs) {
-		Set<Integer> set = new HashSet<>();
 		int[] ind = new int[org.length + 1];
-		for (int i = 1; i <= org.length; i++) {
-			set.add(i);
-			map.put(i, new LinkedList<>());
-		}
 		for (List<Integer> seq : seqs) {
-			for (Integer x : seq) {
-				if (x > org.length || x < 1) return false;
-				set.remove(x);
-			}
+			if (seq.size() == 0) continue;
+			if (seq.get(0) < 1 || seq.get(0) > org.length) return false;
+			map.putIfAbsent(seq.get(0), new LinkedList<>());
 			for (int i = 1; i < seq.size(); i++) {
+				if (seq.get(i) < 1 || seq.get(i) > org.length) return false;
+				map.putIfAbsent(seq.get(i), new LinkedList<>());
 				map.get(seq.get(i - 1)).add(seq.get(i));
 				ind[seq.get(i)]++;
 			}
 		}
-		if (!set.isEmpty()) return false;
+		if (map.size() != org.length) return false;
 		int startPoint = -1;
 		for (int i = 1; i <= org.length; i++) {
 			if (ind[i] == 0) {
@@ -26,15 +22,16 @@ class Solution {
 			}
 		}
 		int index = 0;
-		Queue<Integer> queue = new LinkedList<>();
-		queue.offer(startPoint);
-		while (!queue.isEmpty()) {
-			if (queue.size() != 1) return false;
-			int cur = queue.poll();
+		while (startPoint != -1) {
+			int cur = startPoint;
+			startPoint = -1;
 			if (cur != org[index++]) return false;
 			for (int next : map.get(cur)) {
 				ind[next]--;
-				if (ind[next] == 0) queue.offer(next);
+				if (ind[next] == 0) {
+					if (startPoint != -1) return false;
+					startPoint = next;
+				}
 			}
 		}
 		return index == org.length;
