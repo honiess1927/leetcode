bf4a90a7460a1ec041cede0b4990e55d57f18f79
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-03-25 19:53:36 -0700

    update 301.remove-invalid-parentheses.java

---
+++ 301.remove-invalid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+	Set<String> res = new HashSet<>();
+	int maxLen;
+    public List<String> removeInvalidParentheses(String s) {
+    	dfs(s.toCharArray(), 0, 0, new StringBuilder());
+    	return new LinkedList(res);
+    }
+
+    public void dfs(char[] s, int at, int left, StringBuilder sb) {
+    	if (left < 0) return;
+    	if (at == s.length) {
+    		if (left != 0 || sb.length() < maxLen) return;
+    		if (sb.length() > maxLen) {
+    			res.clear();
+    			maxLen = sb.length();
+    		}
+    		res.add(sb.toString());
+    		return;
+    	}
+    	// No keep this.
+    	dfs(s, at + 1, left, sb);
+    	sb.append(s[at]);
+    	if (s[at] == '(') {
+    		dfs(s, at + 1, left + 1, sb);
+    	} else if (s[at] == ')') {
+    		dfs(s, at + 1, left - 1, sb);
+    	} else {
+    		dfs(s, at + 1, left, sb);
+    	}
+    	sb.deleteCharAt(sb.length() - 1);
+    }
+}
