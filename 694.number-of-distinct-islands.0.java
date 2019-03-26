85dc3bf618de939e44cda1abcf33b89768950c88
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-03-26 10:15:27 -0700

    update 694.number-of-distinct-islands.0.java

--- 694.number-of-distinct-islands.0.java
+++ 694.number-of-distinct-islands.0.java
@@ -8,14 +8,13 @@ class Solution {
         for (int i = 0; i < grid.length; i++) {
         	for (int j = 0; j < grid[0].length; j++) {
         		if (grid[i][j] == 1) {
-        			queue.offer(new int[]{0, 0});
+        			queue.offer(new int[]{i, j});
         			StringBuilder sb = new StringBuilder();
         			while (!queue.isEmpty()) {
         				int x = queue.peek()[0], y = queue.poll()[1];
-        				int xx = x + i, yy = y + j;
-        				if (xx < 0 || yy < 0 || xx >= grid.length || yy >= grid[0].length || grid[xx][yy] == 0) continue;
-        				grid[xx][yy] = 0;
-        				sb.append(x).append(':').append(y).append('|');
+        				if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == 0) continue;
+        				grid[x][y] = 0;
+        				sb.append(x - i).append(':').append(y - j).append('|');
         				for (int k = 0; k < 4; k++) {
         					queue.offer(new int[]{x + pos[k][0], y + pos[k][1]});
         				}
