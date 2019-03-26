b34a9317ecb1f200b71be5f8d87bea9094733b97
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-03-25 21:02:40 -0700

    update 329.longest-increasing-path-in-a-matrix.java

--- 329.longest-increasing-path-in-a-matrix.java
+++ 329.longest-increasing-path-in-a-matrix.java
@@ -9,7 +9,7 @@ class Solution {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (res[i][j] == 0) {
-					dfs(matrix, i, j, matrix[i][j] - 1, 0);
+					dfs(matrix, i, j, matrix[i][j] - 1);
 				}
 				longest = Math.max(longest, res[i][j]);
 			}
@@ -17,16 +17,16 @@ class Solution {
 		return longest;
 	}
 
-	private int dfs(int[][] matrix, int i, int j, int prev, int len) {
-		if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length) return len;
-		if (prev >= matrix[i][j]) return len;
-		if (res[i][j] != 0) return len + res[i][j];
+	private int dfs(int[][] matrix, int i, int j, int prev) {
+		if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length) return 0;
+		if (prev >= matrix[i][j]) return 0;
+		if (res[i][j] != 0) return res[i][j];
 		int longest = 0;
-		longest = Math.max(dfs(matrix, i + 1, j, matrix[i][j], len + 1), longest);
-		longest = Math.max(dfs(matrix, i - 1, j, matrix[i][j], len + 1), longest);
-		longest = Math.max(dfs(matrix, i, j + 1, matrix[i][j], len + 1), longest);
-		longest = Math.max(dfs(matrix, i, j - 1, matrix[i][j], len + 1), longest);
-		res[i][j] = longest - len;
+		longest = Math.max(dfs(matrix, i + 1, j, matrix[i][j]), longest);
+		longest = Math.max(dfs(matrix, i - 1, j, matrix[i][j]), longest);
+		longest = Math.max(dfs(matrix, i, j + 1, matrix[i][j]), longest);
+		longest = Math.max(dfs(matrix, i, j - 1, matrix[i][j]), longest);
+		res[i][j] = ++longest;
 		return longest;
 	}
 }
