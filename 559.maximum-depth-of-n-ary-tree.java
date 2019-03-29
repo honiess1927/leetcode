2873604903f1ad3144ef6a1cff31040a3c5fe7a7
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-03-28 21:33:36 -0700

    update 559.maximum-depth-of-n-ary-tree.java

---
+++ 559.maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,23 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null) return 0;
+        int res = 0;
+        for (Node n : root.children) res = Math.max(maxDepth(n), res);
+        return res + 1;
+    }
+
+}
