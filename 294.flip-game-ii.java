231f4073ae42b1e251002d825209263927e0a166
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-03-31 20:48:55 -0700

    update 294.flip-game-ii.java

---
+++ 294.flip-game-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canWin(String s) {
+    	if (s.length() < 2) return false;
+        for (int i = 0; i < s.length() - 1; i++) {
+        	if (s.charAt(i) == '+' && s.charAt(i + 1) == '+') {
+        		String next = s.substring(0, i) + "--" + s.substring(i + 2);
+        		if (!canWin(next)) return true;
+        	}
+        }
+        return false;
+    }
+}
