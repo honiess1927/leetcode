c8b7bbbb9a2fddd8c77a310a5c44c7e3f295180c
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-03-31 20:51:16 -0700

    update 294.flip-game-ii.java

--- 294.flip-game-ii.java
+++ 294.flip-game-ii.java
@@ -1,12 +1,14 @@
 class Solution {
+	Set<String> set = new HashSet<>();
     public boolean canWin(String s) {
-    	if (s.length() < 2) return false;
+    	if (set.contains(s)) return false;
         for (int i = 0; i < s.length() - 1; i++) {
         	if (s.charAt(i) == '+' && s.charAt(i + 1) == '+') {
         		String next = s.substring(0, i) + "--" + s.substring(i + 2);
         		if (!canWin(next)) return true;
         	}
         }
+        set.add(s);
         return false;
     }
 }
