cdaac67b4db0ed0b29022a9e9d004ebe79ed904d
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-03-31 22:34:02 -0700

    update 45.jump-game-ii.java

--- 45.jump-game-ii.java
+++ 45.jump-game-ii.java
@@ -1,18 +1,17 @@
 class Solution {
-    public int jump(int[] nums) {
-    	if (nums.length == 1) return 0;
-    	int max = 0;
-    	int[] f = new int[nums.length];
-    	for (int i = 0; i < nums.length; i++) {
-    		if (nums[i] + i > max) {
-    			int p = Math.min(nums[i] + i, nums.length - 1);
-    			if (p == nums.length - 1) return f[i] + 1;
-    			while (p > 0 && f[p] == 0) {
-    				f[p] = f[i] + 1;
-    				p--;
-    			}
-    		}
-    	}
-    	return f[nums.length - 1];
-    }
+	public int jump(int[] nums) {
+		int max = 0;
+		int[] f = new int[nums.length];
+		for (int i = 0; i < nums.length && max < nums.length - 1; i++) {
+			if (nums[i] + i > max) {
+				int p = Math.min(nums[i] + i, nums.length - 1);
+				while (p > 0 && f[p] == 0) {
+					f[p] = f[i] + 1;
+					p--;
+				}
+				max = nums[i] + i;
+			}
+		}
+		return f[nums.length - 1];
+	}
 }
