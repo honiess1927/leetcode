0c940e652a0c07d4ccac5c83beae8c810d12fb17
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-04-07 00:40:12 -0700

    update 266.palindrome-permutation.java

--- 266.palindrome-permutation.java
+++ 266.palindrome-permutation.java
@@ -1,15 +1,27 @@
 class Solution {
 	public boolean canPermutePalindrome(String s) {
+
+		// Solution 1: use a map to store all values;
+		// Map<Character, Integer> map = new HashMap<>();
+		// for (char ch : s.toCharArray()) 
+		// 	map.put(ch, map.getOrDefault(ch, 0) + 1);
+		// boolean flag = s.length() % 2 == 1;
+		// for (int value : map.values()) {
+		// 	if (value2 % 2 == 1) {
+		// 		if (!flag) return false;
+		// 		else flag = false;
+		// 	}
+		// }
+		// return true;
+
+		// Solution 2: no second time traverse.
+		int oddCount = 0;
 		Map<Character, Integer> map = new HashMap<>();
-		for (char ch : s.toCharArray()) 
+		for (char ch : s.toCharArray()) {
 			map.put(ch, map.getOrDefault(ch, 0) + 1);
-		boolean flag = s.length() % 2 == 1;
-		for (int value : map.values()) {
-			if (value % 2 == 1) {
-				if (!flag) return false;
-				else flag = false;
-			}
+			if (map.get(ch) % 2 == 1) oddCount++;
+			else oddCount--;
 		}
-		return true;
+		return s.length() % 2 == 1 ? oddCount == 1 : oddCount == 0;
 	}
 }
