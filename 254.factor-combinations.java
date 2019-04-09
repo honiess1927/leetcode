28b82a54b18775d4cc1c6eb12bf667bfae4f028c
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-04-09 11:27:27 -0700

    update 254.factor-combinations.java

---
+++ 254.factor-combinations.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (n == 1) return res;
+        dfs(res, new LinkedList<>(), n, 2);
+        if (res.size() >= 1) res.remove(res.size() - 1);
+        return res;
+    }
+
+    private void dfs(List<List<Integer>> res, LinkedList<Integer> list, int n, int index) {
+    	if (n == 1) {
+    		res.add(new ArrayList(list));
+    		return;
+    	}
+    	for (int i = index; i <= n; i++) {
+    		if (n % i == 0) {
+    			list.add(i);
+    			dfs(res, list, n / i, i);
+    			list.removeLast();
+    		}
+    	}
+    }
+}
