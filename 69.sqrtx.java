bc07683d6750fa83ee303a546ad00060e3e14a3e
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-04-13 12:04:04 -0700

    update 69.sqrtx.java

---
+++ 69.sqrtx.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mySqrt(int x) {
+    	if (x == 0) return x;
+    	int l = 1, r = x;
+    	while (l < r) {
+    		int mid = l + (r - l  + 1) / 2;
+    		if (mid <= x / mid) {
+    			l = mid;
+    		} else {
+    			r = mid - 1;
+    		}
+    	}
+        return l;
+    }
+}
