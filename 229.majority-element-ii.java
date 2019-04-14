bd0fba2d16a05837c257f65b135e39e0794e8e04
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-04-13 18:43:44 -0700

    update 229.majority-element-ii.java

--- 229.majority-element-ii.java
+++ 229.majority-element-ii.java
@@ -2,50 +2,40 @@ class Solution {
 	public List<Integer> majorityElement(int[] nums) {
 		List<Integer> result = new LinkedList<>();
 		if (nums.length == 0) return result;
-		int res = nums[0], count = 0;
-		int counter = 0;
+		int[] can = new int[2], count = new int[2];
 		for (int num : nums) {
-			if (num == res) count++;
-			else {
-				if (count == 0) {
-					res = num;
-					count++;
-				} else {
-					if (counter == 1) {
-						count--;
-						counter = 0;
-					} else {
-						counter++;
-					}
+			if (count[0] == 0) {
+				can[0] = num;
+				count[0]++;
+			} else if (num == can[0]) count[0]++;
+			else if (count[1] == 0) {
+				can[1] = num;
+				count[1]++;
+			} else if (num == can[1]) {
+				count[1]++;
+				if (count[1] > count[0]) {
+					int swap = can[0];
+					can[0] = can[1];
+					can[1] = swap;
+
+					swap = count[0];
+					count[0] = count[1];
+					count[1] = swap;
+				}
+			} else {
+				count[0]--;
+				count[1]--;
+			}
+		}
+		for (int i = 0; i < 2; i++) {
+			int cnt = 0;
+			if (count[i] > 0) {
+				for (int num : nums) {
+					if (can[i] == num) cnt++;
 				}
 			}
+			if (cnt > nums.length / 3) result.add(can[i]);
 		}
-		count = 0;
-		for (int num : nums) {
-			if (num == res) count++;
-		}
-		if (count > nums.length / 3) result.add(res);
-		int i = 0;
-		while (i < nums.length && nums[i] == res) i++;
-		if (i >= nums.length) return result;
-		int newRes = nums[i];
-		count = 0;
-		for (; i < nums.length; i++) {
-			int num = nums[i];
-			if (num == res) continue;
-			if (num == newRes) count++;
-			else {
-				if (count == 0) {
-					newRes = num;
-					count++;
-				} else count--;
-			}
-		}
-		count = 0;
-		for (int num : nums) {
-			if (num == newRes) count++;
-		}
-		if (count > nums.length / 3) result.add(newRes);
 		return result;
 	}
 }
