0714fc573daf15eb7d0cbdea0091e4bb64ee139e
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-04-15 19:44:49 -0700

    update 940.distinct-subsequences-ii.java

--- 940.distinct-subsequences-ii.java
+++ 940.distinct-subsequences-ii.java
@@ -4,9 +4,8 @@ class Solution {
     	// similar to 730.
 		int MOD = 1000000000 + 7;
 		Map<Character, Integer> prevIndex = new HashMap<>();
-		prevIndex.put(s.charAt(0), 1);
-		int sum = 1;
-		for (int i = 1; i < s.length(); i++) {
+		int sum = 0;
+		for (int i = 0; i < s.length(); i++) {
 			char ch = s.charAt(i);
 			int cur = sum + 1;
 			cur = Math.floorMod(cur - prevIndex.getOrDefault(ch, 0), MOD);
