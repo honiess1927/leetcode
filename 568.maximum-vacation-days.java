59b181d55dd8adee29dfa4e1f213e1c934f9ac80
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-04-22 23:26:44 -0700

    update 568.maximum-vacation-days.java

--- 568.maximum-vacation-days.java
+++ 568.maximum-vacation-days.java
@@ -1,20 +1,20 @@
 class Solution {
-	Map<String, Integer> map = new HashMap<>();
+	int[][] memo;
     public int maxVacationDays(int[][] flights, int[][] days) {
+    	memo = new int[days[0].length][flights.length];
         return dfs(flights, days, 0, 0);
     }
 
     private int dfs(int[][] flights, int[][] days, int week, int city) {
     	if (week == days[0].length) return 0;
-    	String key = week + "," + city;
-    	if (map.containsKey(key)) return map.get(key);
+    	if (memo[week][city] != 0) return memo[week][city];
     	int res = days[city][week] + dfs(flights, days, week + 1, city);
     	for (int i = 0; i < flights[0].length; i++) {
     		if (flights[city][i] == 1) {
     			res = Math.max(res, days[i][week] + dfs(flights, days, week + 1, i));
     		}
     	}
-    	map.put(key, res);
+    	memo[week][city] = res;
     	return res;
     }
 }
