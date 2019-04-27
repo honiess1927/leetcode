1af98d594125fcd54f334133aaaec132130dbf55
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-04-26 22:56:12 -0700

    update 375.guess-number-higher-or-lower-ii.java

--- 375.guess-number-higher-or-lower-ii.java
+++ 375.guess-number-higher-or-lower-ii.java
@@ -9,13 +9,12 @@ class Solution {
 		if (l >= r) return 0;
 		if (memo[l][r] != 0) return memo[l][r];
 		memo[l][r] = Integer.MAX_VALUE;
-		for (int i = l; i <= r; i++) {
+		for (int i = l; i < r; i++) {
 			memo[l][r] = Math.min(memo[l][r], i + Math.max(helper(l, i - 1), helper(i + 1, r)));
 		}
 		// print(l, r, memo[l][r]);
 		return memo[l][r];
 	}
-
 
 	private void print(Object... xs) { 
 		for (Object x : xs) {
