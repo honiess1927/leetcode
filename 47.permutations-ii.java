da912f6c83cf0cb97c1052a67a13a691a0047173
honiess1927 <45275162+honiess1927@users.noreply.github.com>
2019-04-29 23:42:47 -0700

    update 47.permutations-ii.java

--- 47.permutations-ii.java
+++ 47.permutations-ii.java
@@ -1,23 +1,27 @@
 class Solution {
 	public List<List<Integer>> permuteUnique(int[] nums) {
-		Set<List<Integer>> res = new HashSet<>();
+		List<List<Integer>> res = new LinkedList<>();
+		Arrays.sort(nums);
 		dfs(res, new LinkedList<>(), nums, new boolean[nums.length]);
 		return new LinkedList<>(res);
 	}
 
-	private void dfs(Set<List<Integer>> res, LinkedList<Integer> list, int[] nums, boolean[] visited) {
+	private void dfs(List<List<Integer>> res, LinkedList<Integer> list, int[] nums, boolean[] visited) {
 		if (list.size() == nums.length) {
 			res.add(new LinkedList(list));
 			return;
 		}
-		for (int i = 0; i < nums.length; i++) {
+		int i = 0;
+		while (i < nums.length) {
 			if (!visited[i]) {
 				list.add(nums[i]);
 				visited[i] = true;
 				dfs(res, list, nums, visited);
 				visited[i] = false;
 				list.removeLast();
-			}
+				i++;
+				while (i < nums.length && nums[i] == nums[i - 1]) i++;
+			} else i++;
 		}
 	}
 
