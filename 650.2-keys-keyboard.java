f8bb79736a90085034f0137e02606fb1f08156e9
Sherlock Dox <45275162+honiess1927@users.noreply.github.com>
2019-05-13 20:38:03 -0700

    update 650.2-keys-keyboard.java

--- 650.2-keys-keyboard.java
+++ 650.2-keys-keyboard.java
@@ -1,15 +1,12 @@
  class Solution {
     public int minSteps(int n) {
-    	if (n < 2) return 0;
     	int[] f = new int[n + 1];
-    	f[0] = 0;
-    	f[1] = 0;
-    	f[2] = 2;
-    	for (int i = 3; i <= n; i++) {
+    	for (int i = 2; i <= n; i++) {
     		f[i] = Integer.MAX_VALUE;
-    		for (int j = 1; j < i; j++) {
+    		for (int j = i - 1; j >= 0; j--) {
     			if (i % j == 0) {
-    				f[i] = Math.min(f[i], f[j] + i / j);
+    				f[i] = f[j] + i / j;
+    				break;
     			}
     		}
     	}
