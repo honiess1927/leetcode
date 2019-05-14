44709319f8ee94bbc58cafe9d9bb1a48058d5807
Sherlock Dox <45275162+honiess1927@users.noreply.github.com>
2019-05-13 19:51:10 -0700

    update 1043.partition-array-for-maximum-sum.java

--- 1043.partition-array-for-maximum-sum.java
+++ 1043.partition-array-for-maximum-sum.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] A, int K) {
         int[] f = new int[A.length + 1];
-        for (int i = 0; i < A.length; i++) {
+        for (int i = 1; i <= A.length; i++) {
         	int max = Integer.MIN_VALUE;
-        	for (int j = 0; j < K && i - j >= 0; j++) {
+        	for (int j = 1; j <= K && i - j >= 0; j++) {
         		max = Math.max(max, A[i - j]);
-        		f[i + 1] = Math.max(f[i + 1], f[i - j] + max * (j + 1));
+        		f[i] = Math.max(f[i], f[i - j] + max * (j));
         	}
         }
         // print(f);
