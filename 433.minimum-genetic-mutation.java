eeb85e7772069b723eae42a3634740ecbc2f2256
Sherlock Dox <45275162+honiess1927@users.noreply.github.com>
2019-05-19 17:30:39 -0700

    update 433.minimum-genetic-mutation.java

--- 433.minimum-genetic-mutation.java
+++ 433.minimum-genetic-mutation.java
@@ -1,80 +1,33 @@
 class Solution {
-    static Map<Character, Integer> map = new HashMap<>();
-    static {
-        map.put('A', 0);
-        map.put('C', 1);
-        map.put('G', 2);
-        map.put('T', 3);
-    }
-    public int minMutation(String start, String end, String[] bank) {
-        Set<Integer> gBank = new HashSet<>();
-        for (String gene : bank) {
-            gBank.add(toInt(gene));
-        }
-        int startInt = toInt(start), endInt = toInt(end);
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(startInt);
+    public int minMutation(String start, String end, String[] gbank) {
+        Set<String> bank = new HashSet<>();
+        bank.addAll(Arrays.asList(gbank));
+        char[] chars = new char[]{'A', 'C', 'G', 'T'};
+        if (!bank.contains(end)) return -1;
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(start);
         int res = 0;
-        // print(startInt, endInt, gBank, "      ");
-        // print(toStr(startInt));
-
         while (!queue.isEmpty()) {
             int len = queue.size();
-            for (int step = 0; step < len; step++) {
-                int cur = queue.poll();
-                // print(toStr(cur));
-                if (cur == endInt) return res;
-                int mask = 0xFFFC;
-                int[] val = new int[]{0, 1, 2, 3};
+            for (int k = 0; k < len; k++) {
+                String cur = queue.poll();
+                if (end.equals(cur)) return res;
+                StringBuilder sb = new StringBuilder(cur);
                 for (int i = 0; i < 8; i++) {
-                    // print(i);
-                    for (int j = 0; j < 4; j++) {
-                        int next = cur & mask | val[j];
-                        // print(toStr(next));
-                        if (gBank.contains(next)) {
-                            queue.offer(next);
-                            gBank.remove(next);
+                    for (char ch : chars) {
+                        if (ch != cur.charAt(i));
+                        sb.setCharAt(i, ch);
+                        String str = sb.toString();
+                        if (bank.contains(str)) {
+                            bank.remove(str);
+                            queue.offer(str);
                         }
-                        val[j] = val[j] << 2;
                     }
-                    // print("    ");
-                    mask = mask << 2 | 3;
+                    sb.setCharAt(i, cur.charAt(i));
                 }
             }
-            // print(gBank,"             ");
             res++;
         }
-
         return -1;
-    }
-
-    private int toInt(String s) {
-        int res = 0;
-        for (char ch : s.toCharArray()) {
-            res = res | map.get(ch);
-            res = res << 2;
-        }
-        return res >> 2;
-    }
-    private void print(Object... xs) { for (Object x : xs) { if (x.getClass().isArray()) { System.out.print(Arrays.toString((int[])x) + " "); } else { System.out.print(x + " "); } } System.out.print(" ");}
-
-    private String toStr(int x) {
-        int mask = 3;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 8; i++) {
-            int tmp = mask & x;
-            switch (tmp) {
-                case 0: sb.append('A');
-                break;
-                case 1: sb.append('C');
-                break;
-                case 2: sb.append('G');
-                break;
-                case 3: sb.append('T');
-            }
-            x = x >>> 2;
-        }
-        return sb.reverse().toString();
     }
 }
